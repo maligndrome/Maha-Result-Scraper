269100bdb77e94ff4096f299be12f8981a5feeca
Yuvraj Singh Bawa <rajayuv@gmail.com>
2016-07-08 02:01:13 +0530

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -2,7 +2,7 @@
 	var globCtr=0;
 	var pagesArray=[];
 	var link='http://mahresult.nic.in/hsc2016/resultview16.asp';
-	var roll=268645;
+	//var roll=268645;
 	var a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 		var b=['a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
 		var c=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -18,7 +18,7 @@
 			return names;
 		}
 	var itrnames=nameGen();
-	var commonNames=['sar','sas','sat','sav','see','sha','she','shi','sho','shy','sit','son','sri','shr','sud','sul','sum','sun','sus','swa','tar','tri','uma','ush','val','vas','vid','vij','vim','abh','adi','ahs','ais','aka','ama','ami','amr','ana','ani','anj','ank','ano','anu','arc','aru','ash','aas','aar','art','ava','bal','bha','cha','che','dam','dar','dee','ant','dev','dip','div','dur','esh','gau','git','gop','gow','ila','ind','jas','jay','jyo','kai','kal','kam','kan','ish','kar','kau','kir','kis','kri','kru','ksh','kum','kun','lak','lal','lat','lav','lax','lee','lel','lil','mad','mal','man','may','mir','mee','moh','muk','med','nee','nil','nik','nis','nit','pad','par','poo','pra','pre','pri','pun','pur','pus','rac','rad','raj','ran','ras','rat','res','ris','rit','riy','roh','ruk','rup','san','sar'];
+	var commonNames=['sar','sas','sat','sav','see','sha','she','shi','sho','shy','sit','son','sri','shr','sud','sur','sul','sum','sun','sus','swa','tar','tri','uma','ush','val','vas','vid','vij','vim','abh','adi','ahs','ais','aka','ama','ami','amr','ana','ani','anj','ank','ano','anu','arc','aru','ash','aas','aar','art','ava','bal','bha','cha','che','dam','dar','dee','ant','dev','dip','div','dur','esh','gau','git','gop','gow','ila','ind','jas','jay','jyo','kai','kal','kam','kan','ish','kar','kau','kir','kis','kri','kru','ksh','kum','kun','lak','lal','lat','lav','lax','lee','lel','lil','mad','mal','man','may','mir','mee','moh','muk','med','nee','nil','nik','nis','nit','pad','par','poo','pra','pre','pri','pun','pur','pus','rac','rad','raj','ran','ras','rat','res','ris','rit','riy','roh','ruk','rup','san','sar'];
 	scrapeData = function(roll) {
 		$.post('http://mahresult.nic.in/hsc2016/resultview16.asp',
 			{regno:'M'+roll,mname:itrnames[globCtr]}, 
@@ -33,8 +33,9 @@
 					}
 				}else{
 					console.log(data);
+					console.log('$$$$');
 					globCtr=0;
-					//scrapeData(++roll);
+					scrapeData(++roll);
 				}
 			}
 		);
@@ -44,7 +45,7 @@
 			{regno:'M'+roll,mname:commonNames[globCtr]}, 
 			function(data){ 
 				if(data.indexOf('bg-danger')>-1){
-					if(globCtr==131) {
+					if(globCtr==133) {
 						console.log('moving on...')
 						globCtr=0;
 						scrapeDataCommon(++roll);
@@ -54,10 +55,11 @@
 					}
 				}else{
 					console.log(data);
-					console.log(commonNames[globCtr]);
+					console.log('$$$$$');
 					globCtr=0;
-					//scrapeData(++roll);
+					scrapeData(++roll);
 				}
 			}
 		);
 	};
+	
