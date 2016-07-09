e896941ddab08a486616197b4f54d7eb81a0a30d
Yuvraj Singh Bawa <rajayuv@gmail.com>
2016-07-09 16:34:32 +0530

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -20,7 +20,7 @@ var level=0;
 			return names;
 		}
 	var itrnames=nameGen();
-	var commonNames=['sar','sas','sat','sav','see','sha','she','shi','sho','shy','sit','son','sri','shr','sud','sur','sul','sum','sun','sus','swa','tar','tri','uma','ush','val','vas','vid','vij','vim','abh','adi','ahs','ais','aka','ama','ami','amr','ana','ani','anj','ank','ano','anu','arc','aru','ash','aas','aar','art','ava','bal','bha','cha','che','dam','dar','dee','ant','dev','dip','div','dur','esh','gau','git','gop','gow','ila','ind','jas','jay','jyo','kai','kal','kam','kan','ish','kar','kau','kir','kis','kri','kru','ksh','kum','kun','lak','lal','lat','lav','lax','lee','lel','lil','mad','mal','man','may','mir','mee','moh','muk','med','nee','nil','nik','nis','nit','pad','par','poo','pra','pre','pri','pun','pur','pus','rac','rad','raj','ran','ras','rat','res','ris','rit','riy','roh','ruk','rup','san','sar','tab'];
+	var commonNames=['sar','sas','sat','sav','see','sha','she','shi','sho','shy','sit','son','sri','shr','sud','sur','sul','sum','sun','sus','swa','tar','tri','uma','ush','val','vas','vid','vij','vim','abh','adi','ahs','ais','aka','ama','ami','amr','ana','ani','anj','ank','ano','anu','arc','aru','ash','aas','aar','art','ava','bal','bha','cha','che','dam','dar','dee','ant','dev','dip','div','dur','esh','gau','git','gop','gow','ila','ind','jas','jay','jyo','kai','kal','kam','kan','ish','kar','kau','kir','kis','kri','kru','ksh','kum','kun','lak','lal','lat','lav','lax','lee','lel','lil','mad','mal','man','may','mir','mee','moh','muk','med','nee','nil','nik','nis','nit','pad','pal','par','poo','pra','pre','pri','pun','pur','pus','rac','rad','raj','ran','ras','rat','res','ris','rit','riy','roh','ruk','rup','san','sar','tab'];
 	scrapeData = function(roll) {
 		$.post('http://mahresult.nic.in/hsc2016/resultview16.asp',
 			{regno:'M'+roll,mname:itrnames[globCtr]}, 
@@ -47,7 +47,7 @@ var level=0;
 			{regno:'M'+roll,mname:commonNames[globCtr]}, 
 			function(data){
 				if(data.indexOf('bg-danger')>-1){
-					if(globCtr==132) {
+					if(globCtr==134) {
 						logFile+=('##'+commonNames[globCtr]+':roll'+'##')+'Not found';
 						globCtr=0;
 						if(--maxPages){scrapeDataCommon(++roll);}
