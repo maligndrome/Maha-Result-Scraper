18a02cbced0cf46e4c06e230bfffcadb97780269
Yuvraj Singh Bawa <rajayuv@gmail.com>
2016-07-11 14:45:47 +0530

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,25 +1,10 @@
 var level=0;
 	var globCtr=0;
-	var pagesArray=[]; var maxPages=1000;
+	var pagesArray=[]; var maxPages=50;
 	var link='http://mahresult.nic.in/hsc2016/resultview16.asp';
 	//var roll=268645;
 	var file = "data:text/plain;charset=utf-8,";
 	var logFile = '';
-	var a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-		var b=['a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
-		var c=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-		nameGen= function() {
-			var names=[];
-			for(var i=0;i<26;i++){
-				for(var j=0;j<26;j++){
-					for(var k=0;k<26;k++){
-						names.push(a[k]+b[i]+c[j]);
-					}
-				}
-			}
-			return names;
-		}
-	var itrnames=nameGen();
 	var commonNames=['sar','sas','sat','sav','see','sha','she','shi','sho','shy','sit','son','sri','shr','sud','sur','sul','sum','sun','sus','swa','tar','tri','uma','ush','val','vas','vid','vij','vim','abh','adi','ahs','ais','aka','ama','ami','amr','ana','ani','anj','ank','ano','anu','arc','aru','ash','aas','aar','art','ava','bal','bha','cha','che','dam','dar','dee','ant','dev','dip','div','dur','esh','gau','git','gop','gow','ila','ind','jas','jay','jyo','kai','kal','kam','kan','ish','kar','kau','kir','kis','kri','kru','ksh','kum','kun','lak','lal','lat','lav','lax','lee','lel','lil','mad','mal','man','may','mir','mee','moh','muk','med','nee','nil','nik','nis','nit','pad','pal','par','poo','pra','pre','pri','pun','pur','pus','rac','rad','raj','ran','ras','rat','res','ris','rit','riy','roh','ruk','rup','san','sar','tab'];
 	scrapeData = function(roll) {
 		$.post('http://mahresult.nic.in/hsc2016/resultview16.asp',
@@ -62,14 +47,16 @@ var level=0;
 					if(--maxPages){scrapeDataCommon(++roll);}
 					else {
 					    var encoded = encodeURIComponent(logFile);
-					    file += encoded;
 					    var a = document.createElement('a');
-					    a.href = file;
+					    a.href = file+encoded;
 					    a.target   = '_blank';
 					    a.download = 'data-from-'+(roll-1000)+'-to-'+roll;
 					    document.body.appendChild(a);
 					    a.click();
 					    a.remove();
+					    maxPages=50;
+					    logFile='';
+					    scrapeDataCommon(++roll);
 					}
 				}
 			}
