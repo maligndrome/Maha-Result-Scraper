caf97c2b2b328f1d0f1ad30b940fa56553372de5
Yuvraj Singh Bawa <rajayuv@gmail.com>
2016-07-12 19:32:51 +0530

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -5,7 +5,7 @@ var level=0;
 	//var roll=268645;
 	var file = "data:text/plain;charset=utf-8,";
 	var logFile = '';
-	var commonNames=['sar','sas','sat','sav','see','sha','she','shi','sho','shy','sit','son','sri','shr','sud','sur','sul','sum','sun','sus','swa','tar','tri','uma','ush','val','vas','vid','vij','vim','abh','adi','ahs','ais','aka','ama','ami','amr','ana','ani','anj','ank','ano','anu','arc','aru','ash','aas','aar','art','ava','bal','bha','cha','che','dam','dar','dee','ant','dev','dip','div','dur','esh','gau','git','gop','gow','ila','ind','jas','jay','jyo','kai','kal','kam','kan','ish','kar','kau','kir','kis','kri','kru','ksh','kum','kun','lak','lal','lat','lav','lax','lee','lel','lil','mad','mal','man','may','mir','mee','moh','muk','med','nee','nil','nik','nis','nit','pad','pal','par','poo','pra','pre','pri','pun','pur','pus','rac','rad','raj','ran','ras','rat','res','ris','rit','riy','roh','ruk','rup','san','sar','tab'];
+	var commonNames=['sar','sah','sas','sak','sat','sam','sad','sav','see','sha','she','shi','sho','shy','sit','son','sri','shr','sud','sur','sul','sum','sun','sus','swa','tar','tri','uma','ush','val','vas','vid','vij','vim','abh','adi','ahs','ais','aka','ama','ami','amr','ana','ani','anj','ank','ano','anu','arc','aru','ash','aas','aar','art','ava','bal','bha','cha','chh','chi','che','dam','dar','dee','day','ant','dev','dip','div','dur','esh','gau','git','gop','gow','ila','ind','jas','jay','jyo','kai','kal','kam','kaj','kan','ish','kar','kau','kir','kis','kri','kru','ksh','kum','kun','lak','lal','lat','lav','lax','lee','lel','lil','mad','mal','man','may','mir','mee','moh','mon','mou','muk','med','nee','nil','nik','nis','nit','pad','pal','par','poo','pra','pre','pri','pun','pur','pus','rac','rad','raj','ran','ram','ras','rat','res','ris','rit','riy','roh','ruk','rup','san','sar','tab','dha','var','vin','vai','yam','yas','yag','rud'];
 	scrapeData = function(roll) {
 		$.post('http://mahresult.nic.in/hsc2016/resultview16.asp',
 			{regno:'M'+roll,mname:itrnames[globCtr]}, 
@@ -32,8 +32,8 @@ var level=0;
 			{regno:'M'+roll,mname:commonNames[globCtr]}, 
 			function(data){
 				if(data.indexOf('bg-danger')>-1){
-					if(globCtr==134) {
-						logFile+=('##'+commonNames[globCtr]+':'+roll+'##')+'Not found';
+					if(globCtr>153) {
+						logFile+=('##'+roll+'##')+'Not found';
 						globCtr=0;
 						if(--maxPages){scrapeDataCommon(++roll);}
 						else {
